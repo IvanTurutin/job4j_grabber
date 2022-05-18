22dd26643f06678e25525f6ed70561deb3cf120f
Ivan <ivan.turutin@gmail.com>
2022-05-18 20:41:28 +0300

    3. Шаблонизатор. [#855]

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -1,9 +1,14 @@
 package ru.job4j.template;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
+import ru.job4j.tdd.*;
 
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Map;
 
 public class GeneratorTest {
@@ -12,7 +17,7 @@ public class GeneratorTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenKeyMissing() {
         Generator gen = new StringGenerator();
-        String str = "I am a ${name}, Who are ${subject}?";
+        String str = "I am ${name}, Who are ${subject}?";
         Map<String, String> map = Map.of("name", "Ivan", "brand", "Nike");
         gen.produce(str, map);
     }
@@ -21,8 +26,18 @@ public class GeneratorTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenRedundandKey() {
         Generator gen = new StringGenerator();
-        String str = "I am a ${name}, Who are ${subject}?";
+        String str = "I am ${name}, Who are ${subject}?";
         Map<String, String> map = Map.of("name", "Ivan", "subject", "you", "brand", "Nike");
         gen.produce(str, map);
+    }
+
+    @Ignore
+    @Test
+    public void whenProduce() {
+        Generator gen = new StringGenerator();
+        String str = "I am ${name}, Who are ${subject}?";
+        Map<String, String> map = Map.of("name", "Ivan", "subject", "you");
+        String rst = gen.produce(str, map);
+        assertTrue("I am Ivan, Who are you?".equals(rst));
     }
 }
